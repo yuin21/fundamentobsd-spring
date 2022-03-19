4611ac18fb800a9314d985e89a289ad4a48077b1
Alex Yupanqui <ayupanqui@bsdsystem.com.pe>
2022-03-19 06:42:53 -0500

    First commit yuin

---
+++ src/main/java/com/fundamentobsd/springboot/fundamentobsd/FundamentobsdApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentobsd.springboot.fundamentobsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentobsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentobsdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentobsd/springboot/fundamentobsd/FundamentobsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentobsd.springboot.fundamentobsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentobsdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
